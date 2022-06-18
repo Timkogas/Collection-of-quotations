db5e61e108859230dc4d3e3c4549525ed20d257a
Timur Ermolaev <timkogas@gmail.com>
2022-06-18 09:40:35 +0300

    #2 реализовал отображение страницы AddQoute и прописал стили

--- src/components/AddQuoteForm/AddQuoteForm.js
+++ src/components/AddQuoteForm/AddQuoteForm.js
@@ -1,9 +1,33 @@
 import './AddQuoteForm.css'
+import Categories from '../../Categories';
 
-function AddQuoteForm() {
+function AddQuoteForm(props) {
   return (
     <>
-    
+      <form className='add_block' onSubmit={props.AddQuote}>
+        <h1>Add quote</h1>
+
+        <select onChange={props.selectChange} name='select' value={props.selectValue}>
+          {Categories.map((category)=>{
+            return (
+              <option value={category.id}>{category.title}</option>
+            )
+          })}
+        </select>
+
+        <div>
+          <p>Author</p>
+          <input onChange={props.inputChange} name='input' value={props.inputValue}></input>
+        </div>
+
+        <div>
+          <p>Quote text</p>
+          <textarea onChange={props.textAreaChange} name='textArea' value={props.textAreaValue}></textarea>
+        </div>
+
+
+        {props.inputValue && props.textAreaValue ? <button>Add</button> : null}
+      </form>
     </>
   );
 }
--- src/containers/AddQuote/AddQuote.js
+++ src/containers/AddQuote/AddQuote.js
@@ -1,10 +1,36 @@
 import './AddQuote.css'
 import AddQuoteForm from '../../components/AddQuoteForm/AddQuoteForm';
+import { useState } from 'react'
+import axios from 'axios';
+import Preloader from '../../UI/Preloader/Preloader'
+import { useNavigate } from 'react-router-dom';
 
 function AddQuote() {
+  const [selectValue, setSelectValue] = useState('')
+  const [inputValue, setInputValue] = useState('')
+  const [textAreaValue, setTextAreaValue] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const inputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const textAreaChange = (e) => {
+    setTextAreaValue(e.target.value)
+  }
+
+  const navigate = useNavigate();
+
+  const AddQuote = async (e) => {
+    setLoading(true)
+    e.preventDefault();
+    setLoading(false)
+  }
+
   return (
     <>
-    
+      <Preloader showPlaceholder={loading}/>
+      <AddQuoteForm/>
     </>
   );
 }
