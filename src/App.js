92891d3e36f0727aea83690833b1ad5082f5f2d7
Timur Ermolaev <timkogas@gmail.com>
2022-06-18 09:04:10 +0300

    #2 взял прелоадер из прошлых работ, реализовал роутеры

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import QuotesPage from "./containers/QuotesPage/QuotesPage";
 import AddQuote from "./containers/AddQuote/AddQuote";
 import EditQuote from "./containers/EditQuote/EditQuote";
 import Layout from "./components/Layout/Layout";
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route element={<Layout/>}>
+            <Route path='/'element={<QuotesPage/>} />
+            <Route path='/quotes'element={<QuotesPage/>}/>
+            <Route path='/add-quote'element={< AddQuote/>} />
+            <Route path='/quotes/:id/edit'element={<EditQuote/>}/>
+            <Route path='/quotes/:id'element={<QuotesPage/>} />
+          </Route>
+        <Route/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
