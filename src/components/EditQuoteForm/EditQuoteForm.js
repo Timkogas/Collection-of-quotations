5dcc7e3798ce2592245979191ea8b6b928b234fc
Timur Ermolaev <timkogas@gmail.com>
2022-06-18 09:44:12 +0300

    #2 реализовал отображение страницы EditQoute и прописал стили

--- src/components/EditQuoteForm/EditQuoteForm.js
+++ src/components/EditQuoteForm/EditQuoteForm.js
@@ -1,9 +1,33 @@
 import './EditQuoteForm.css'
+import Categories from '../../Categories';
 
-function EditQuoteForm() {
+function EditQuoteForm(props) {
   return (
     <>
-    
+      <form className='edit_block' onSubmit={props.AddQuote}>
+        <h1>Edit quote</h1>
+
+        <select onChange={props.selectChange} name='select' value={props.selectValue}>
+          {Categories.map((category)=>{
+            return (
+              <option value={category.id}>{category.title}</option>
+            )
+          })}
+        </select>
+
+        <div>
+          <p>Author</p>
+          <input onChange={props.inputChange} name='input' value={props.inputValue}></input>
+        </div>
+
+        <div>
+          <p>Quote text</p>
+          <textarea onChange={props.textAreaChange} name='textArea' value={props.textAreaValue}></textarea>
+        </div>
+
+
+        {props.inputValue && props.textAreaValue ? <button>Add</button> : null}
+      </form>
     </>
   );
 }
--- src/containers/EditQuote/EditQuote.js
+++ src/containers/EditQuote/EditQuote.js
@@ -1,10 +1,36 @@
 import './EditQuote.css'
 import EditQuoteForm from '../../components/EditQuoteForm/EditQuoteForm';
+import { useState } from 'react'
+import axios from 'axios';
+import Preloader from '../../UI/Preloader/Preloader'
+import { useNavigate } from 'react-router-dom';
 
 function EditQuote() {
+  const [selectValue, setSelectValue] = useState('')
+  const [inputValue, setInputValue] = useState('')
+  const [textAreaValue, setTextAreaValue] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const inputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const textAreaChange = (e) => {
+    setTextAreaValue(e.target.value)
+  }
+
+  const navigate = useNavigate();
+
+  const AddQuote = async (e) => {
+    setLoading(true)
+    e.preventDefault();
+    setLoading(false)
+  }
+
   return (
     <>
-    
+      <Preloader showPlaceholder={loading}/>
+      <EditQuoteForm/>
     </>
   );
 }
